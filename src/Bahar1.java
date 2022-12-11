f65c9024e49913624afeadfb0baeec9f41f186be
Bahar Taskin <cakirbhr7@gmail.com>
2022-12-11 20:39:48 +0300

    answer for first question

---
+++ src/Bahar1.java
@@ -0,0 +1,10 @@
+public class Bahar1 {
+
+    public static void main(String[] args) {
+        // 1. When do we use single if statement?
+        /*
+        Single if: can be used creating condition for 1 possible output
+         */
+    }
+
+}
