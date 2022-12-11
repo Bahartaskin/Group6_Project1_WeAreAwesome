6d5fa344a13e736c984d642a65043b1a7b00234b
Kamuran Sahin <kamuransahin.c11@gmail.com>
2022-12-11 23:07:48 +0300

    description of String charAt method write the initials of the given strings

--- src/files/CharAtMethod.java
+++ src/files/CharAtMethod.java
@@ -4,27 +4,27 @@ public class CharAtMethod {
 
     public static void main(String[] args) {
 
-        String firstName= "Wooden";
-        String lastName= "Spoon";
+        String firstName=  "Wooden";
+        String lastName=  "Spoon";
 
         char first=firstName.charAt(0);
         char last=lastName.charAt(0);
 
         System.out.println(first+"."+last);
+
+        /*
+
+    1. Can you explain charAt method?
+
+    Answer: charAt(index): returns the character at the given index, returns char
+
+    2. Create a class to display your initials
+	initial:
+		firstName = "Wooden";
+		lastName = "Spoon";
+
+	output:
+		W.S
+         */
     }
-
-/*
-  1. Can you explain charAt method?
-
-  The charAt() method returns the character at the specified index in a string.
-
-  The index of the first character is 0, the second character is 1, and so on.
-
-            2. Create a class to display your initials
-            initial:
-    firstName = "Wooden";
-    lastName = "Spoon";
-
-    output:
-    W.S*/
 }
