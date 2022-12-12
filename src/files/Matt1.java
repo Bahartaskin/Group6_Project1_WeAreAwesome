1b39c053e88d1fda9cf3c16eebbc79d4e5ce4355
Matthew Stubbs <mattstbbs@outlook.com>
2022-12-12 20:30:30 +0300

    matt_branch2_Answer1 posted on .java class named as Matt1

---
+++ src/files/Matt1.java
@@ -0,0 +1,17 @@
+package src.files;
+
+public class Matt1 {
+
+    public static void main(String[] args) {
+
+/*
+        1.Can you explain substring method?
+
+                Ans.1_:  substring(beginningIndex , endingIndex): creates substring starting from the given beginning
+                 index till the given ending index (ending index is excluded)
+        */
+
+
+
+    }
+}
