faa9213c705fe232759c47ef312836a7e88a6735
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2022-12-12 09:21:40 +0300

    Updated NumbersToArray.java file (added array info at the end)

--- src/files/NumbersToArray.java
+++ src/files/NumbersToArray.java
@@ -14,3 +14,22 @@ public class NumbersToArray {
         System.out.println("Numbers stored : " + Arrays.toString(numbers));
     }
 }
+/*
+1. Can you explain Array?
+       Array is a special variable that can contain multiple elements
+	   Array's size is fixed
+	   Arrays support primitives & non-primitives
+
+	   Declaring array:
+	   		DataType[] variableName;
+
+   		Initializing array:
+   			If we do not know the exact data:
+   				1. DataType[] variableName= new DataType[length]
+   					int[] nums = new int[5];
+
+			If we know the exact data:
+				2. DataType[] variableName = {data1, data2, dat3 ...}
+
+2. Write a program that asks user to enter a number 10 times and store all user entered numbers in an array
+*/
