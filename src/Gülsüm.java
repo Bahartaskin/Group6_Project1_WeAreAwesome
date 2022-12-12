80a0c879767444f4f881472106b08d715ed42476
w10 <glsmh.16@gmail.com>
2022-12-13 00:07:12 +0300

    gülsüm branch created

---
+++ src/Gülsüm.java
@@ -0,0 +1,27 @@
+public class Gülsüm {
+   /* 1- The action of linking things together by using “+” operator
+    There must be a string of text on the right or left side of the “+” operator
+
+    */
+
+ public static void main(String[] args) {
+
+        String student_name = "Bahar";
+        int age = 32;
+        char gender = 'F';
+        String school_name = "Cydeo";
+        String phone_number = "(###)-###-####";
+        boolean full_time = true;
+        double GPA = 2.5;
+
+        System.out.println("student_name = " + student_name);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("school_name = " + school_name);
+        System.out.println("phone_number = " + phone_number);
+        System.out.println("full_time = " + full_time);
+        System.out.println("GPA = " + GPA);
+
+    }
+
+}
